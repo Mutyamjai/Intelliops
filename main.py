ed87355701bef474e603577ddb745b55d17a26e3
Mutyamjai <22ucs130@lnmiit.ac.in>
2026-02-24 06:25:22 +0530

    model is added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ingestion import load_and_convert
 from convertion import create_daily_order
 from convertion import orders_per_day
 from convertion import get_weekly_aggregation
+from model import main_ml_pipeline
 def main():
     shopify_df = load_and_convert(r"C:\Users\Mutyam Jai\Desktop\python_ai\data\shopify_orders.csv","shopify")
     amazon_df = load_and_convert(r"C:\Users\Mutyam Jai\Desktop\python_ai\data\amazon_orders.csv","amazon")
@@ -10,5 +11,6 @@ def main():
     final_df = shopify_df._append(amazon_df,ignore_index = True)._append(manual_df,ignore_index = True)
     daily_order_df = create_daily_order(final_df)
     df = get_weekly_aggregation(final_df)
+    df = main_ml_pipeline(final_df)
 if __name__ == "__main__":
     main()
